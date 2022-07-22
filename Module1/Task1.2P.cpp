ba6e9ac472703f92054e7a2f6ebc666efacc6446
rickydodd <63101193+rickydodd@users.noreply.github.com>
2022-07-22 14:08:05 +1000

    Create Task1.2P.cpp

---
+++ Module1/Task1.2P.cpp
@@ -0,0 +1,38 @@
+const uint8_t led_pin = 2;
+const uint8_t motion_pin = 3;
+
+bool isMotion;
+
+void setup()
+{
+  pinMode(led_pin, OUTPUT);
+  pinMode(motion_pin, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(motion_pin), handle, CHANGE);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  delay(500);
+}
+
+void handle()
+{
+  isMotion = digitalRead(motion_pin);
+  
+  Serial.print("Is motion? : ");
+  Serial.println(isMotion ? "True" : "False");
+  
+  if(isMotion == true)
+  {
+    digitalWrite(led_pin, HIGH);
+    Serial.println("LED is on.");
+  }
+  else
+  {
+    digitalWrite(led_pin, LOW);
+    Serial.println("LED is off.");
+  }
+}
